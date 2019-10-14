93c2412c1d4b7a9de6adeef2b12aff1027a4003c
doxuanviet <doxuanviet1996@kaist.ac.kr>
2019-10-15 02:06:10 +0900

    Merge branch 'master' of https://github.com/vietanhle97/CS442KATIE
    # Conflicts:
    #	app/src/main/java/com/example/cs442katie/SignUpActivity.kt

--- app/src/main/java/com/example/cs442katie/MainActivity.kt
+++ app/src/main/java/com/example/cs442katie/MainActivity.kt
@@ -2,8 +2,15 @@ package com.example.cs442katie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.auth.Token
+import com.google.firebase.iid.FirebaseInstanceId
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,5 +21,24 @@ class MainActivity : AppCompatActivity() {
         btn.setOnClickListener(View.OnClickListener {
 
         })
+
+        FirebaseInstanceId.getInstance().instanceId
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    return@OnCompleteListener
+                }
+
+                // Get new Instance ID token
+                val token = task.result?.token as String
+
+                val userId = FirebaseAuth.getInstance().uid as String
+
+                FirebaseDatabase.getInstance().reference.child("token").child(userId).setValue(token)
+
+                // Log and toast
+//                val msg = getString(R.string.msg_token_fmt, token)
+//                Log.d("current token", msg)
+//                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+            })
     }
 }
---
+++ app/src/main/java/com/example/cs442katie/MyFirebaseMessagingService.kt
@@ -0,0 +1,62 @@
+package com.example.cs442katie
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.IBinder
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    private val TAG = "FirebaseService"
+
+    /**
+     * this method will be triggered every time there is new FCM Message.
+     */
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        Log.d(TAG, "Refreshed token: $token")
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        // sendRegistrationToServer(token)
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.d(TAG, "From: " + remoteMessage.from)
+
+        if(remoteMessage.notification != null) {
+            Log.d(TAG, "Notification Message Body: ${remoteMessage.notification?.body}")
+            sendNotification(remoteMessage.notification?.body)
+        }
+    }
+
+    private fun sendNotification(body: String?) {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            putExtra("Notification", body)
+        }
+
+        var pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+        val notificationSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+
+        var notificationBuilder = NotificationCompat.Builder(this,"Notification")
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle("Push Notification FCM")
+            .setContentText(body)
+            .setAutoCancel(true)
+            .setSound(notificationSound)
+            .setContentIntent(pendingIntent)
+
+        var notificationManager: NotificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(0, notificationBuilder.build())
+    }
+}
